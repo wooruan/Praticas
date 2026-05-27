c6831277c55b813a4f7206276062cf17af0cc44c
Pedro Ruan O. Moura <pedroruanoliveiramoura@gmail.com>
2026-05-26 21:10:57 -0300

    Delete Python_Exercícios directory

--- Python_Exercícios/ex17.py
+++
@@ -1,8 +0,0 @@
-import math
-ca = float((input("qual o cumprimento do cateto adjascente: ")))
-co = float((input("qual o cumprimento do oposto: ")))
-h = (ca ** (2/1) + co ** (2/1)) ** (1/2)
-print (h)
-hi = math.hypot(ca, co)
-print ("o cumprimento da ipotenusa é: {:.2f} ou {:.2f}".format(h, hi))
-5
